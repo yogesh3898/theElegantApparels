b8362577c406244103e2b72d94e351c699dbc513
Yogesh <yogeshwaran3898@gmail.com>
2022-07-24 21:36:14 +0530

    bug fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,25 +15,35 @@ function Header({
         const toggler=document.getElementsByClassName('capture-navbar')
         toggler[0].classList.toggle('collapse')
     }
-    const clickHandler=()=>{
+    const clickHandler=(event)=>{
         if(user){
             auth.signOut()
             navigate('/')
         }
+        const toggler=document.getElementsByClassName('capture-navbar')
+        if(!toggler[0].classList.contains('collapse')){
+            toggler[0].classList.toggle('collapse')
+        }
+    }
+    const togglerExtra=()=>{
+        const toggler=document.getElementsByClassName('capture-navbar')
+        if(!toggler[0].classList.contains('collapse')){
+            toggler[0].classList.toggle('collapse')
+        }
     }
   return (
     <nav className='navbar bg-dark navbar-expand-lg navbar-dark'>
-        <div className='navbar-brand'><Link className='link' to='/'>The Elegant Apparels</Link></div>
+        <div className='navbar-brand' onClick={togglerExtra}><Link className='link' to='/'>The Elegant Apparels</Link></div>
         <button onClick={toggler} className='navbar-toggler' data-toggle='collapse' data-target='#collapsible'>
             <span className='navbar-toggler-icon'>
             </span>
         </button>
-        <div className='navbar-collapse capture-navbar'>
+        <div className='navbar-collapse capture-navbar collapse'>
             <ul className='navbar-nav'>
-                <li className='nav-item'><Link className='nav-link men' to='/idealFor/Men'>Men
+                <li className='nav-item' onClick={togglerExtra}><Link className='nav-link men' to='/idealFor/Men'>Men
             
                 </Link></li>
-                <li className='nav-item'><Link className='nav-link women' to='/idealFor/Women'>Women
+                <li className='nav-item' onClick={togglerExtra}><Link className='nav-link women' to='/idealFor/Women'>Women
                 
                 </Link></li>
                 {/* <li className='nav-item'><Link className='nav-link kids' to='/idealFor/Kids'>Kids
@@ -45,12 +55,12 @@ function Header({
             </ul>
             <ul className='navbar-nav'>
                 {(user?.email==='yogesh@admin.com')?(
-                    <li className='nav-item'><Link className='nav-link' to='/upload-product'>Upload product</Link></li>
+                    <li className='nav-item' onClick={togglerExtra}><Link className='nav-link' to='/upload-product'>Upload product</Link></li>
                 ):null}
-                <li className='nav-item user-name'>Hi {user?user?.email.split('@')[0]:'guest'} !</li>
+                <li className='nav-item user-name' onClick={togglerExtra}>Hi {user?user?.email.split('@')[0]:'guest'} !</li>
                 <li onClick={clickHandler} className='nav-item'><Link className='nav-link' to={!user && '/login'}>{user?'Sign out':'Sign in'}</Link></li>
-                <li className='nav-item'><Link className='nav-link' to={user?'/orders':'/login'}>Orders</Link></li>
-                <li className='nav-item'><Link className='nav-link' to={user?'/basket':'/login'}><FaBabyCarriage /><span className='cart-number'>{basket.length}</span></Link></li>
+                <li className='nav-item' onClick={togglerExtra}><Link className='nav-link' to={user?'/orders':'/login'}>Orders</Link></li>
+                <li className='nav-item' onClick={togglerExtra}><Link className='nav-link' to={user?'/basket':'/login'}><FaBabyCarriage /><span className='cart-number'>{basket.length}</span></Link></li>
             </ul>
         </div>
     </nav>
